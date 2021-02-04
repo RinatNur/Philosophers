8e2a940a1026d6d6d812e6ea1633bdc6656a5ba7
Rinat <nuretdin1405@gmail.com>
2021-02-04 13:09:15 +0300

    added philo_two

---
+++ philo_two/print_utils.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jheat <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/31 18:59:34 by jheat             #+#    #+#             */
+/*   Updated: 2021/01/31 18:59:36 by jheat            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+ssize_t		ft_write(int fd, const void *buf)
+{
+	int		len;
+	int		ret;
+
+	len = ft_strlen(buf);
+	((ret = write(fd, buf, len)) == -1)
+	? print_error("Error in function write", 3) : 0;
+	return (ret);
+}
+
+void		print_action(t_phil *all, char *str)
+{
+	long int		time;
+
+	check_life_time(all);
+	sem_wait(g_print);
+	time = get_time() - g_data.start_time;
+	ft_putnbr_fd(time, 1);
+	ft_write(1, " ");
+	ft_putnbr_fd((all->index), 1);
+	ft_write(1, str);
+	if (g_data.is_dead == 1)
+		sem_post(g_print);
+	sem_post(g_print);
+}
+
+void		print_action_dead(t_phil *all, char *str)
+{
+	long int		time;
+
+	time = get_time() - g_data.start_time;
+	ft_putnbr_fd(time, 1);
+	ft_write(1, " ");
+	ft_putnbr_fd((all->index), 1);
+	ft_write(1, str);
+}
+
+void		print_error(char *str, int code)
+{
+	write(1, str, ft_strlen(str));
+	unlink_sem();
+	exit(code);
+}
---
+++ philo_two/utils.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jheat <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/31 19:00:53 by jheat             #+#    #+#             */
+/*   Updated: 2021/01/31 19:01:11 by jheat            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+void			unlink_sem(void)
+{
+	sem_unlink("g_forks");
+	sem_unlink("g_print");
+}
+
+long int		get_time(void)
+{
+	struct timeval		tv;
+	long int			timestamp;
+
+	gettimeofday(&tv, NULL);
+	timestamp = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	return (timestamp);
+}
+
+size_t			ft_strlen(const char *s)
+{
+	size_t		i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+int				atoi_mini(int *nbr, char *str)
+{
+	if (*str == '\0')
+		return (1);
+	*nbr = 0;
+	while (*str && '0' <= *str && *str <= '9')
+	{
+		*nbr = *nbr * 10 + *str - '0';
+		str++;
+	}
+	if (*str != '\0' || *nbr == 0)
+		return (1);
+	return (0);
+}
+
+void			ft_putnbr_fd(long int n, int fd)
+{
+	char		sym;
+
+	if (n > 9)
+		ft_putnbr_fd(n / 10, fd);
+	sym = (char)(n % 10 + '0');
+	write(fd, &sym, 1);
+}
